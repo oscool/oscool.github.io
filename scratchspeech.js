ce74fee795b663225b4f17996488a941e1fd8acb
OS_Cool_ <95204181+oscool@users.noreply.github.com>
2021-12-19 13:29:28 -0500

    Update scratchspeech.js

--- scratchspeech.js
+++ scratchspeech.js
@@ -1,6 +1,5 @@
 class ScratchFetch {
     constructor() {}
-
     getInfo() {
         return {
             id: "scratchspeech",
@@ -8,42 +7,39 @@ class ScratchFetch {
             blocks: [
                 {
                     opcode: "s",
-                    blockType: "command",
-                    text: "Speech Recognization",
-                },
-                {
-                    opcode: "t",
                     blockType: "reporter",
-                    text: "Transcript",
-                },
+                    text: "Speech Recognization",
+                }
             ],
         };
     }
 
     s({}) {
+        // reset transcript
+        var transcript = ""
         // new speech recognition object
         var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
         var recognition = new SpeechRecognition();
 
         // This runs when the speech recognition service starts
-        recognition.onstart = function () {};
+        recognition.onstart = function () {
+            var talking = true
+        };
 
         recognition.onspeechend = function () {
             // when user is done speaking
             recognition.stop();
+            var talking = false
         };
 
         // This runs when the speech recognition service returns result
         recognition.onresult = (event) => {
             var transcript = event.results[0][0].transcript;
-            var confidence = event.results[0][0].confidence;
         };
-
+        
         // start recognition
-        recognition.start();
-    }
-    t({}) {
-        return transcript;
+        recognition.start()
+        return transcript
     }
 }
 Scratch.extensions.register(new ScratchFetch());
