a9ab42323e69cd2c6b5db3782592cb8fbde98eb4
OS_Cool_ <95204181+oscool@users.noreply.github.com>
2021-12-24 21:26:41 -0500

    Create scratchapi.js

---
+++ scratchapi.js
@@ -0,0 +1,29 @@
+class ScratchAPI {
+    constructor() {}
+    getInfo() {
+        return {
+            id: "scratchAPI",
+            name: "Scratch API",
+            blocks: [
+                {
+                    opcode: "msg_count",
+                    blockType: "reporter",
+                    text: "Message Count of [user]",
+                    "arguments": {
+                                "user": {
+                                    "type": "string",
+                                    "defaultValue": "OS_Cool_"
+                                },
+                            }
+                }
+            ],
+        };
+    }
+
+    msg_count({user}) {
+        url = "https://api.scratch.mit.edu/users/" + user + "/messages/count/"
+        return fetch(url).then(response => response.text())
+    }
+}
+Scratch.extensions.register(new ScratchAPI());
+
