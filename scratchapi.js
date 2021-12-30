10853dea1e38799bf5f94b75e48725ee4dd30041
OS_Cool_ <95204181+oscool@users.noreply.github.com>
2021-12-30 17:48:25 -0500

    Update scratchapi.js

--- scratchapi.js
+++ scratchapi.js
@@ -26,7 +26,8 @@ class ScratchAPI {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             "Accept-Language": "en"
         }
-        const url = "https://cors-proxy.htmldriven.com/?url=https://api.scratch.mit.edu/users/" + user + "/messages/count/"
+        const corsproxy = "http://alloworigin.com/get?url="
+        const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/messages/count/"
         return fetch(url, headers).then(response => response.text())
     }
 }
