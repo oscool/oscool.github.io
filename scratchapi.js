e1ba854caea8107eeb4a70a8b3d95d2441bc6aca
OS_Cool_ <95204181+oscool@users.noreply.github.com>
2021-12-30 19:44:08 -0500

    Update scratchapi.js

--- scratchapi.js
+++ scratchapi.js
@@ -6,7 +6,7 @@ class ScratchAPI {
             name: "Scratch API",
             blocks: [
                 {
-                    opcode: "msg_count",
+                    opcode: "user_msg_count",
                     blockType: "reporter",
                     text: "Get [user]'s Message Count",
                     "arguments": {
@@ -17,28 +17,112 @@ class ScratchAPI {
                             }
                 },
                 {
-                    opcode: "user",
+                    opcode: "user_profile",
                     blockType: "reporter",
-                    text: "Get [user]",
+                    text: "Get [user]'s User Profile",
                     "arguments": {
                                 "user": {
                                     "type": "string",
                                     "defaultValue": "OS_Cool_"
                                 },
                             }
+                },
+                {
+                    opcode: "user_favs",
+                    blockType: "reporter",
+                    text: "Get [user]'s Favourites",
+                    "arguments": {
+                                "user": {
+                                    "type": "string",
+                                    "defaultValue": "OS_Cool_"
+                                },
+                            }
+                },
+                {
+                    opcode: "user_followers",
+                    blockType: "reporter",
+                    text: "Get [user]'s Followers",
+                    "arguments": {
+                                "user": {
+                                    "type": "string",
+                                    "defaultValue": "OS_Cool_"
+                                },
+                            }
+                },
+                {
+                    opcode: "user_following",
+                    blockType: "reporter",
+                    text: "Get [user]'s Following",
+                    "arguments": {
+                                "user": {
+                                    "type": "string",
+                                    "defaultValue": "OS_Cool_"
+                                },
+                            }
+                },
+                {
+                    opcode: "user_projects",
+                    blockType: "reporter",
+                    text: "Get [user]'s Projects",
+                    "arguments": {
+                                "user": {
+                                    "type": "string",
+                                    "defaultValue": "OS_Cool_"
+                                },
+                            }
+                },
+                {
+                    opcode: "user_project",
+                    blockType: "reporter",
+                    text: "Get [user]'s Project [id]",
+                    "arguments": {
+                                "user": {
+                                    "type": "string",
+                                    "defaultValue": "OS_Cool_"
+                                },
+                                "id": {
+                                    "type": "string",
+                                    "defaultValue": "588007972"
+                                }
+                            }
                 }
             ],
         };
     }
 
-    msg_count({user}) {
+    user_msg_count({user}) {
         const corsproxy = "https://cors-anywhere.herokuapp.com/"
         const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/messages/count/"
         return fetch(url).then(response => response.text())
     }
-    user({user}) {
+    user_profile({user}) {
         const corsproxy = "https://cors-anywhere.herokuapp.com/"
         const url = corsproxy + "https://api.scratch.mit.edu/users/" + user
+        return fetch(url).then(response => response.text())
+    }
+    user_favs({user}) {
+        const corsproxy = "https://cors-anywhere.herokuapp.com/"
+        const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/favorites/"
+        return fetch(url).then(response => response.text())
+    }
+    user_followers({user}) {
+        const corsproxy = "https://cors-anywhere.herokuapp.com/"
+        const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/followers/"
+        return fetch(url).then(response => response.text())
+    }
+    user_following({user}) {
+        const corsproxy = "https://cors-anywhere.herokuapp.com/"
+        const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/following/"
+        return fetch(url).then(response => response.text())
+    }
+    user_projects({user}) {
+        const corsproxy = "https://cors-anywhere.herokuapp.com/"
+        const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/projects/"
+        return fetch(url).then(response => response.text())
+    }
+    user_project({user,id}) {
+        const corsproxy = "https://cors-anywhere.herokuapp.com/"
+        const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/projects/" + id
         return fetch(url).then(response => response.text())
     }
 }
