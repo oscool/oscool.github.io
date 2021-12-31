9ad777d32ecf2d54dcbfb4f11498204e3c9f1e21
OS_Cool_ <95204181+oscool@users.noreply.github.com>
2021-12-30 19:22:22 -0500

    Update scratchapi.js

--- scratchapi.js
+++ scratchapi.js
@@ -8,7 +8,18 @@ class ScratchAPI {
                 {
                     opcode: "msg_count",
                     blockType: "reporter",
-                    text: "Message Count of [user]",
+                    text: "Get [user]'s Message Count",
+                    "arguments": {
+                                "user": {
+                                    "type": "string",
+                                    "defaultValue": "OS_Cool_"
+                                },
+                            }
+                },
+                {
+                    opcode: "user",
+                    blockType: "reporter",
+                    text: "Get [user]",
                     "arguments": {
                                 "user": {
                                     "type": "string",
@@ -23,6 +34,11 @@ class ScratchAPI {
     msg_count({user}) {
         const corsproxy = "https://cors-anywhere.herokuapp.com/"
         const url = corsproxy + "https://api.scratch.mit.edu/users/" + user + "/messages/count/"
+        return fetch(url).then(response => response.text())
+    }
+    user({user}) {
+        const corsproxy = "https://cors-anywhere.herokuapp.com/"
+        const url = corsproxy + "https://api.scratch.mit.edu/users/" + user
         return fetch(url).then(response => response.text())
     }
 }
